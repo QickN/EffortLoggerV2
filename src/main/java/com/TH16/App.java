4adbba755bf3989cc2a7f29c3372138fb87a5651
QickN <nickquam38@gmail.com>
2023-10-16 14:52:24 -0700

    Added some UI improvements

--- src/main/java/com/TH16/App.java
+++ src/main/java/com/TH16/App.java
@@ -7,9 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -45,30 +51,46 @@ public class App extends Application {
         Label instruction1 = new Label("1. When you start a new activity, press the \"Start an Activity\" button.");
         Button startActivityBtn = new Button("Start an Activity");
         Button defectLogConsoleBtn = new Button("Proceed to Defect Log Console");
-        VBox centerTopBox = new VBox(10, instruction1, startActivityBtn, defectLogConsoleBtn);
-    
+        HBox buttonsHBox = new HBox(10, startActivityBtn, defectLogConsoleBtn); 
+        buttonsHBox.setAlignment(Pos.CENTER); 
+        VBox centerTopBox = new VBox(10, instruction1, buttonsHBox);
+
         Label instruction2 = new Label("2. Select the project, life cycle step, effort category, and deliverable from the following lists:");
         ComboBox<String> projectCombo = new ComboBox<>();
         projectCombo.getItems().addAll("Business Project");
         Label projectLabel = new Label("Project:");
         HBox projectHBox = new HBox(10, projectLabel, projectCombo);
-    
-        // You can add more ComboBoxes for Life Cycle Step, Effort Category, etc., just like projectCombo above
+        
+        // Adding more ComboBoxes for Life Cycle Step, Effort Category, etc.
+        ComboBox<String> lifeCycleCombo = new ComboBox<>();
+        lifeCycleCombo.getItems().addAll("Select Life Cycle Step...");
+        Label lifeCycleLabel = new Label("Life Cycle Step:");
+        HBox lifeCycleHBox = new HBox(10, lifeCycleLabel, lifeCycleCombo);
+
+        ComboBox<String> effortCategoryCombo = new ComboBox<>();
+        effortCategoryCombo.getItems().addAll("Select Effort Category...");
+        Label effortCategoryLabel = new Label("Effort Category:");
+        HBox effortCategoryHBox = new HBox(10, effortCategoryLabel, effortCategoryCombo);
+
+        VBox comboBoxVBox = new VBox(10, projectHBox, lifeCycleHBox, effortCategoryHBox);
     
         Label instruction3 = new Label("3. Press the \"Stop this Activity\" to generate an effort log entry using the attributes above.");
         Button stopActivityBtn = new Button("Stop this Activity");
         Button effortLogEditorBtn = new Button("Proceed to Effort Log Editor");
-        VBox centerBottomBox = new VBox(10, instruction2, projectHBox, instruction3, stopActivityBtn, effortLogEditorBtn);
-    
-        VBox centerBox = new VBox(20, centerTopBox, centerBottomBox);
-        centerBox.setSpacing(10);
-        root.setCenter(centerBox);
+        
+        VBox centerBottomBox = new VBox(10, instruction3, stopActivityBtn, effortLogEditorBtn);
     
         // Bottom content
         Label instruction4 = new Label("4. Unless you are done for the day, it is best to perform steps 1 and 2 above before resuming work.");
         VBox bottomBox = new VBox(instruction4);
         bottomBox.setSpacing(10);
         root.setBottom(bottomBox);
+
+        VBox centerBox = new VBox(20, centerTopBox, instruction2, comboBoxVBox, centerBottomBox);
+        centerBox.setSpacing(10);
+        centerBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT))); // Add borders
+        root.setCenter(centerBox);
+    
     
         // Create tabs
         TabPane tabPane = new TabPane();
